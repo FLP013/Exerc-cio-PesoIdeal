a8659e14d3a8868da076541fefa9d911c5e6acbf
FLP013 <felliperibeiro9@gmail.com>
2024-10-12 17:02:15 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double Altura, PesoIdeal;
+string Sexo;
+
+Console.Clear();
+
+Console.WriteLine("--- PESO IDEAL ---");
+Console.Write("Me diga á sua altura: ");
+Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Me diga o seu sexo: ");
+Sexo = (Console.ReadLine()!).ToUpper();
+
+if (Sexo == "M")
+{
+PesoIdeal = Altura * 72.7 - 58.0;
+}
+else 
+{
+    PesoIdeal = Altura * 62.1 - 44.7;
+}
+
+Console.WriteLine($"Seu peso ideal é de {PesoIdeal:N1}kg.");
